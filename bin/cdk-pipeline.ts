9d2cebc369302994cfc7b5cd903ad7d9ad615667
Binh Ngo <bnngo97@gmail.com>
2023-05-01 14:35:49 -0700

    revised bootstrap account and pipeline stack env

--- bin/cdk-pipeline.ts
+++ bin/cdk-pipeline.ts
@@ -5,7 +5,13 @@ import { CdkpipelinesDemoPipelineStack } from '../lib/cdk-pipelines-pipeline-sta
 const app = new App();
 
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
-  // no need to create multiple accounts through control tower
+  // complete steps for control tower https://aws.amazon.com/blogs/aws/aws-control-tower-set-up-govern-a-multi-account-aws-environment/
+  // bootstrapped with specified profile and account number in code block below
+
+  // npx cdk bootstrap \
+  // --profile 539460444185_AWSAdministratorAccess \
+  // --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess \
+  // aws://539460444185/us-east-1
   env: { account: '539460444185', region: 'us-east-1' },
 });
 
